d4ea3a11d7bd1ee8b0b1bf19887eac3f0347f907
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2016-10-03 18:57:16 +0200

    Update common.js

--- common.js
+++ common.js
@@ -25,7 +25,7 @@ module.exports = {
     'valid-jsdoc': 'off',
     'class-methods-use-this': 'off',
     'dot-location': ['error', 'property'],
-    'no-magic-numbers': ['error', { 'ignore': [-1, 0, 1, 10, 100, 1000] }],
+    'no-magic-numbers': ['error', { 'ignore': [-1, 0, 1, 2, 10, 100, 1000] }],
     'no-warning-comments': 'off',
     'no-alert': 'off',
     'no-invalid-this': 'off',
