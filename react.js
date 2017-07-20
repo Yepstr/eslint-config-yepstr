6a7cf458d5f535e76c8fefbba995034d9ea714fb
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2017-07-20 09:48:36 +0200

    typo

--- react.js
+++ react.js
@@ -47,7 +47,7 @@ module.exports = {
         'componentWillUpdate',
         'componentDidUpdate',
         'componentWillUnmount',
-        '/^handle.+$',
+        '/^handle.+$/',
         '/^on.+$/',
         '/^get.+$/',
         '/^render.+$/',
